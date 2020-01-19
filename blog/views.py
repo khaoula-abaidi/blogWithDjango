7d0c8adadab85b7f1f9c83ae2c33c8b6bb3a06e1
khaoula-abaidi <abaidik@gmail.com>
2020-01-19 14:12:17 +0100

    templates with bootstrap && url with django

--- blog/views.py
+++ blog/views.py
@@ -17,11 +17,11 @@ posts = [
 
 
 def home(request):
-    context ={
+    context = {
         'posts': posts
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
